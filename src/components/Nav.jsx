060869912f58bc64fe8913c2b2249ca4e69fca55
zepe0 <aitor.zepe@gmail.com>
2024-07-22 13:20:56 +0200

    start

---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <>
+      <div >
+        
+          <ul className="Nav">
+              <li className="itemnav">
+                  <a href="" target="_blank">
+              
+                    <img src="../public/ins.png" className="img_menu" alt="img_menu" />
+                  </a>
+              </li>
+              <li className="itemnav">
+                  <a href="" target="_blank">
+              
+                    <img src="../public/mask.png" className="img_menu" alt="img_menu" />
+                  </a>
+              </li>
+              <li className="itemnav">
+                  <a href="" target="_blank">
+              
+                    <img src="../public/tarjeta.png" className="img_menu" alt="img_menu" />
+                  </a>
+              </li>
+          </ul>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
