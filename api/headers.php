15394e20d69a787b98ecc83165105adb19db359c
zepe <aitor.zepe@gmail.com>
2024-08-05 20:30:58 +0200

    "Updated CORS headers and Vite config"

--- api/headers.php
+++ api/headers.php
@@ -1,5 +1,7 @@
 <?php
 // Agrega los encabezados CORS
-header('Access-Control-Allow-Origin: http://localhost:5173'); // Permite solicitudes desde tu frontend
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost", // El puerto en el que está corriendo tu servidor PHP
+        target: "http://localhost/jasomaki/api", // Cambia esto al puerto correcto de tu servidor PHP
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Ajusta si es necesario
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
