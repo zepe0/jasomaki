e7f6d21c3e7301bd1c910105a61c03633083241f
zepe <aitor.zepe@gmail.com>
2024-09-06 21:56:36 +0200

    "Updated Env.php with new database credentials, fixed typo in Loading.css, and commented out server proxy in vite.config.js"

--- api/model/Env.php
+++ api/model/Env.php
@@ -1,10 +1,10 @@
 <?php
 class Env
 {
-    const DB_HOST = 'localhost';
-    const DB_USER = 'root';
-    const DB_PASS = "";
-    const DB_NAME = 'jasomaki';
+    const DB_HOST = 'sql104.infinityfree.com';/* localhost'; */
+    const DB_USER = "if0_37252972";/* 'root'; */
+    const DB_PASS = "Z01061990z.";
+    const DB_NAME = 'if0_37252972_jasomaki';/*  'jasomaki'; */
     const SECRET_JWT = 'JASOMAKI';
     const STRIPE_PUBLIC ="pk_test_51Poq0zCpoWoxyqfn98r7bUexZcSfykOpjKwLyEw2pY3Ly3fBVvraqK0ivDt8tTnvRjNdWIv7SD9mdmnxzLrGTBJl00mC6X7tE7";
     Const STRIPE_KEY="sk_test_51Poq0zCpoWoxyqfnVamWHgpvISEzqt7XCUDE4mnj2RZnipfVTm9YvT7K4plkniqgi32HGlA568BsXJY73HFT3wHD00BOTQ5KRH";
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   
-  server: {
+  /* server: {
     proxy: {
       "/api": {
         target: "https://localhost/jasomaki/api", // Cambia esto al puerto correcto de tu servidor PHP
@@ -12,5 +12,5 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  },
+  }, */
 });
