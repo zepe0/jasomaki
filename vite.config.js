95c9f2a47f00677eb9ee66cbff6d8de8ff65bea0
zepe0 <aitor.zepe@gmail.com>
2024-12-22 12:41:05 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
 
-  server: {
+ /*  server: {
     proxy: {
       "/api": {
         target: "https://localhost/jasomaki/api", // Cambia esto al puerto correcto de tu servidor PHP
@@ -12,7 +12,8 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  },
+  }, */
+  base: "/jasomaki/",
   build: {
     chunkSizeWarningLimit: 1000, // Ajusta el límite (en KB). Por ejemplo, 1000 KB (1 MB)
   },
